6398499f3aa6821017e32645cc1891a0aee4e51c
astugit <eitanm3006@gmail.com>
2025-11-27 13:58:14 -0800

    Update listen.py

--- capslock/listen.py
+++ capslock/listen.py
@@ -7,7 +7,7 @@ from zoneinfo import ZoneInfo
 
 async def run_client():
     #uri = "ws://localhost:8000/ws"
-    uri = "wss://0.0.0.0:8080"
+    uri = "wss://0.0.0.0:8080/ws"
 
     with open("counts.txt", "a") as file:
         async with websockets.connect(uri) as websocket:
